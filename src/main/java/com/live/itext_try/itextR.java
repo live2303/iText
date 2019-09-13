12886d857d37050f482fd20601de2cf97ce6fb86
T43422 <T43422@E15005799.banxico.org.mx>
2019-09-13 11:54:57 -0500

    everything

--- src/main/java/com/live/itext_try/itextR.java
+++ src/main/java/com/live/itext_try/itextR.java
@@ -15,11 +15,18 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
 import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
@@ -39,7 +46,12 @@ public class itextR {
 
         PdfFont regular = PdfFontFactory.createFont(FontConstants.HELVETICA);
         PdfFont bold = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
+        PdfFont italic = PdfFontFactory.createFont(FontConstants.HELVETICA_OBLIQUE);
         DeviceRgb BMcolor = new DeviceRgb(0, 39, 91);
+        DeviceRgb disableColor = new DeviceRgb(224, 224, 224);
+        SolidLine line = new SolidLine(0.3f);
+        line.setColor(disableColor);
+
         // Creating a PdfDocument       
         PdfDocument pdfDoc = new PdfDocument(writer);
         IEventHandler handler = new Background(pdfDoc, "templante.pdf");
@@ -47,7 +59,9 @@ public class itextR {
         PdfPage page = pdfDoc.addNewPage();
         Document document = new Document(pdfDoc); //, PageSize.LETTER
         document.setMargins(60, 40, 50, 40);
-        document.setFont(regular);
+        document.setFont(regular).setFontSize(11);
+        
+        Text noInfo = new Text("(No existe información)").setFont(italic).setFontColor(disableColor);
         
         /*FOLIO*/
         Text folio = new Text((String)param.get("folioEvento")).setFont(bold).setFontSize(18).setUnderline();
@@ -65,29 +79,83 @@ public class itextR {
         Rectangle rectangleFR = new Rectangle(40, 630, 300, 50);
         pdfCanvas.rectangle(rectangleFR);
         canvas = new Canvas(pdfCanvas, pdfDoc, rectangleFR);       
-        canvas.add(new Paragraph().add(new Text("Fecha: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(fecha).add("\n").add(new Text("Responsable: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(responsable).setTextAlignment(TextAlignment.LEFT));
+        canvas.add(new Paragraph().add(new Text("Fecha del evento: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(fecha).add("\n").add(new Text("Responsable de seguimiento: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(responsable).setTextAlignment(TextAlignment.LEFT).setFixedLeading(20f));
         canvas.close();
-        document.add(new Paragraph("\n\n\n\n\n\n\n\n"));
+        document.add(new Paragraph("\n\n\n\n\n\n\n\n\n"));
         /*Descripción*/
         Text desc = new Text((String)param.get("descripcionEvento")).setFont(regular).setFontSize(11);
-        document.add(new Paragraph().add(new Text("Descripción: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(desc).setTextAlignment(TextAlignment.JUSTIFIED)); //.setFixedPosition(40, 450, null)
+        document.add(new Paragraph().add(new Text("Descripción: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(desc).setTextAlignment(TextAlignment.JUSTIFIED).setMarginBottom(15f)); //.setFixedPosition(40, 450, null)
         
         /*Causas*/
         Text causas = new Text((String)param.get("descripcionCausa")).setFont(regular).setFontSize(11);
-        document.add(new Paragraph().add(new Text("Descripción de la(s) causa(s): ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(causas).setTextAlignment(TextAlignment.JUSTIFIED)).add(); //me falta el pinshi salto pero ya me voy alv
+        document.add(new Paragraph().add(new Text("Descripción de la(s) causa(s): ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(causas).setTextAlignment(TextAlignment.JUSTIFIED).setMarginBottom(15f));
         
         /*Consecuencias*/
         Text consecuencias = new Text((String)param.get("descripcionConsecuencia")).setFont(regular).setFontSize(11);
-        document.add(new Paragraph().add(new Text("Descripción de la(s) consecuencia(s) o afectación: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(consecuencias).setTextAlignment(TextAlignment.JUSTIFIED));
+        document.add(new Paragraph().add(new Text("Descripción de la(s) consecuencia(s) o afectación: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(consecuencias).setTextAlignment(TextAlignment.JUSTIFIED).setMarginBottom(15f));
         
         /*AccInmediatas*/
         Text AcInmediatas =new Text((String)param.get("accionesInmediatas")).setFont(regular).setFontSize(11);
-        document.add(new Paragraph().add(new Text("Acciones de atención inmediata que se llevaron a cabo al presentarse el evento: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(AcInmediatas).setTextAlignment(TextAlignment.JUSTIFIED));
+        document.add(new Paragraph().add(new Text("Acciones de atención inmediata que se llevaron a cabo al presentarse el evento: ").setFont(bold).setFontSize(12).setFontColor(BMcolor)).add(AcInmediatas).setTextAlignment(TextAlignment.JUSTIFIED).setMarginBottom(15f));
 
         /*Impacto */
-        document.add(new Paragraph().add(new Text("Impacto/Consecuencia").setFont(bold).setFontSize(12).setFontColor(BMcolor)).setTextAlignment(TextAlignment.JUSTIFIED));
+        document.add(new Paragraph().add(new Text("Impacto/Consecuencia:").setFont(bold).setFontSize(12).setFontColor(BMcolor)).setTextAlignment(TextAlignment.JUSTIFIED));
         
-  
+        
+        Paragraph oper = new Paragraph(new Text("Operativo ").setFont(bold).setFontColor(param.get("operativo").equals("X") ?  BMcolor : disableColor)).setFontSize(11);
+        if(!param.get("operativoAfectacion").equals("")) oper.add(new Text(String.format("(Nivel de afectación: %s)\n",(String)param.get("operativoAfectacion"))).setFont(italic)); else oper.add(noInfo);
+        if(!param.get("operativoDetalle").equals("")) oper.add(new Text(String.format(" %s",(String)param.get("operativoDetalle"))).setFont(regular));
+        
+        Paragraph mon = new Paragraph(new Text("Monetario ").setFont(bold).setFontColor(param.get("monetario").equals("X") ?  BMcolor : disableColor)).setFontSize(11);
+        if(!param.get("monetarioAfectacion").equals("")) mon.add(new Text(String.format("(Nivel de afectación: %s)\n",(String)param.get("monetarioAfectacion"))).setFont(italic)); else mon.add(noInfo);
+        if(!param.get("monetarioDetalle").equals("")) mon.add(new Text(String.format(" %s",(String)param.get("monetarioDetalle"))).setFont(regular));
+        
+        Paragraph rep = new Paragraph(new Text("Reputación ").setFont(bold).setFontColor(param.get("reputacion").equals("X") ?  BMcolor : disableColor)).setFontSize(11);
+        if(!param.get("reputacionAfectacion").equals("")) rep.add(new Text(String.format("(Nivel de afectación: %s)\n",(String)param.get("reputacionAfectacion"))).setFont(italic)); else rep.add(noInfo);
+        if(!param.get("reputacionDetalle").equals("")) rep.add(new Text(String.format(" %s",(String)param.get("reputacionDetalle"))).setFont(regular));
+        
+        Paragraph salud = new Paragraph(new Text("Salud ").setFont(bold).setFontColor(param.get("salud").equals("X") ?  BMcolor : disableColor)).setFontSize(11);
+        if(!param.get("saludAfectacion").equals("")) salud.add(new Text(String.format("(Nivel de afectación: %s)\n",(String)param.get("saludAfectacion"))).setFont(italic)); else salud.add(noInfo);
+        if(!param.get("saludDetalle").equals("")) salud.add(new Text(String.format(" %s",(String)param.get("saludDetalle"))).setFont(regular));
+        
+        Paragraph medA = new Paragraph(new Text("Medio Ambiente ").setFont(bold).setFontColor(param.get("medioAmbiente").equals("X") ?  BMcolor : disableColor)).setFontSize(11).setMarginBottom(15f);
+        if(!param.get("medioAfectacion").equals("")) medA.add(new Text(String.format("(Nivel de afectación: %s)\n",(String)param.get("medioAfectacion"))).setFont(italic)); else medA.add(noInfo);
+        if(!param.get("medioDetalle").equals("")) medA.add(new Text(String.format(" %s",(String)param.get("medioDetalle"))).setFont(regular));
+        
+        document.add(oper).add(mon).add(rep).add(salud).add(medA);
+        
+        /*Acciones*/
+        document.add(new Paragraph().add(new Text("Acciones correctivas o preventivas:").setFont(bold).setFontSize(12).setFontColor(BMcolor)).setTextAlignment(TextAlignment.JUSTIFIED));
+        int maxAccions = 3;
+        for (int i = 1; i <= maxAccions; i++) {
+            if(!param.get("descripcionAccion"+i).equals("")){
+                
+                Table table = new Table(UnitValue.createPercentArray(new float[]{1,1})).useAllAvailableWidth(); 
+//                table.addCell(new Cell().setFont(regular).add(String.format("Responsable: %s",param.get("responsable"+i))).setTextAlignment(TextAlignment.LEFT).setBorder(Border.NO_BORDER));
+//                table.addCell(new Cell().setFont(italic).add(String.format("Fecha: %s",param.get("fecha"+i))).setTextAlignment(TextAlignment.RIGHT).setBorder(Border.NO_BORDER));
+
+                table.addCell(new Cell().add(new Paragraph().add(new Text("Responsable: ").setFont(bold).setFontColor(BMcolor)).add(new Text(String.format("%s",param.get("responsable"+i))).setFont(regular)).setTextAlignment(TextAlignment.LEFT)).setBorder(Border.NO_BORDER));
+                table.addCell(new Cell().add(new Paragraph().add(new Text("Fecha: ").setFont(bold).setFontColor(BMcolor)).add(new Text(String.format("%s",param.get("fecha"+i))).setFont(italic)).setTextAlignment(TextAlignment.RIGHT)).setBorder(Border.NO_BORDER));
+                document.add(table);
+                Text accion_d = new Text((String)param.get("descripcionAccion"+i)).setFont(regular).setFontSize(11);
+                Text accion_c = new Text((String)param.get("comentarios"+i)).setFont(regular).setFontSize(11);
+                document.add(new Paragraph().add(new Text("Descripción: ").setFont(bold).setFontSize(11).setFontColor(BMcolor)).add(accion_d).setTextAlignment(TextAlignment.JUSTIFIED));
+                document.add(new Paragraph().add(new Text("Comentarios del seguimiento: ").setFont(bold).setFontSize(11).setFontColor(BMcolor)).add(accion_c).setTextAlignment(TextAlignment.JUSTIFIED));
+
+
+                if(i<maxAccions) document.add( new LineSeparator(line).setWidthPercent(90).setMarginBottom(5f).setMarginTop(5f).setHorizontalAlignment(HorizontalAlignment.CENTER));
+            }
+        }
+        
+        /**/
+        Table tableF = new Table(UnitValue.createPercentArray(new float[]{1,1})).useAllAvailableWidth().setMarginTop(25f);
+        tableF.addCell(new Cell().add("Estado del evento").setFont(bold).setFontSize(8).setTextAlignment(TextAlignment.CENTER).setBorder(Border.NO_BORDER).setFontColor(BMcolor));
+        if(!param.get("fechaSeguimiento").equals("")) tableF.addCell(new Cell().add("Fecha de cierre").setFont(bold).setFontSize(8).setTextAlignment(TextAlignment.CENTER).setBorder(Border.NO_BORDER).setFontColor(BMcolor)); else tableF.addCell(new Cell().setBorder(Border.NO_BORDER));
+        tableF.addCell(new Cell().setFont(bold).add(String.format("%s",param.get("estadoAtencion"))).setTextAlignment(TextAlignment.CENTER).setBorder(Border.NO_BORDER));
+        tableF.addCell(new Cell().setFont(regular).add(String.format("%s",param.get("fechaSeguimiento"))).setTextAlignment(TextAlignment.CENTER).setBorder(Border.NO_BORDER));
+        
+        document.add(tableF);
+        
         document.close();
     }
     
--- src/main/java/com/live/itext_try/main.java
+++ src/main/java/com/live/itext_try/main.java
@@ -17,31 +17,43 @@ public class main {
     public static void main(String args[]) throws Exception {
         // Creating a PdfWriter       
         Map parameters = new HashMap();
-        parameters.put("fechaEvento", "19 de julio de 2019");
-        parameters.put("folioEvento", "2019100");
-        parameters.put("responsableSeguimiento", "Pedro Antonio Paredes Hernández");
-        parameters.put("descripcionEvento", "DESCEl viernes 19 de julio, el Sr. Salomón Guerrero García, Subgerente Administración y Operación Banxico de Santander reportó mediante correo electrónico lo siguiente: Informarles que en la ruta https://extranet.banxico.org.mx/SAM/ConsultaAutenticacion?execution=e1s1 donde nuestras sucursales consultan el dictamen de las piezas y realizan el pago de aquellas que resultaron auténticas, el recibo R2018206719 aparece como “Procede reembolso” debiendo decir “Pieza Pagada”, ya que el importe se le pago al cliente desde el pasado 10 de agosto de 2018 y el comprobante de pago se cargó a SAM ese mismo día. El 12 de junio del presente se presentó el cliente a nuestra sucursal argumentando que no se le había pagado. El personal de la sucursal al consultar el dictamen y observar que el estatus dice “Procede reembolso”, duplicó el pago al cliente.Se trata de una pieza de $500.");
-        parameters.put("descripcionCausa", "CAUSAEl viernes 19 de julio, el Sr. Salomón Guerrero García, Subgerente Administración y Operación Banxico de Santander reportó mediante correo electrónico lo siguiente: Informarles que en la ruta https://extranet.banxico.org.mx/SAM/ConsultaAutenticacion?execution=e1s1 donde nuestras sucursales consultan el dictamen de las piezas y realizan el pago de aquellas que resultaron auténticas, el recibo R2018206719 aparece como “Procede reembolso” debiendo decir “Pieza Pagada”, ya que el importe se le pago al cliente desde el pasado 10 de agosto de 2018 y el comprobante de pago se cargó a SAM ese mismo día. El 12 de junio del presente se presentó el cliente a nuestra sucursal argumentando que no se le había pagado. El personal de la sucursal al consultar el dictamen y observar que el estatus dice “Procede reembolso”, duplicó el pago al cliente.Se trata de una pieza de $500.");
-        parameters.put("descripcionConsecuencia", "CONSECUENCIAEl viernes 19 de julio, el Sr. Salomón Guerrero García, Subgerente Administración y Operación Banxico de Santander reportó mediante correo electrónico lo siguiente: Informarles que en la ruta https://extranet.banxico.org.mx/SAM/ConsultaAutenticacion?execution=e1s1 donde nuestras sucursales consultan el dictamen de las piezas y realizan el pago de aquellas que resultaron auténticas, el recibo R2018206719 aparece como “Procede reembolso” debiendo decir “Pieza Pagada”, ya que el importe se le pago al cliente desde el pasado 10 de agosto de 2018 y el comprobante de pago se cargó a SAM ese mismo día. El 12 de junio del presente se presentó el cliente a nuestra sucursal argumentando que no se le había pagado. El personal de la sucursal al consultar el dictamen y observar que el estatus dice “Procede reembolso”, duplicó el pago al cliente.Se trata de una pieza de $500.");
-        parameters.put("accionesInmediatas", "ACC_InmEl viernes 19 de julio, el Sr. Salomón Guerrero García, Subgerente Administración y Operación Banxico de Santander reportó mediante correo electrónico lo siguiente: Informarles que en la ruta https://extranet.banxico.org.mx/SAM/ConsultaAutenticacion?execution=e1s1 donde nuestras sucursales consultan el dictamen de las piezas y realizan el pago de aquellas que resultaron auténticas, el recibo R2018206719 aparece como “Procede reembolso” debiendo decir “Pieza Pagada”, ya que el importe se le pago al cliente desde el pasado 10 de agosto de 2018 y el comprobante de pago se cargó a SAM ese mismo día. El 12 de junio del presente se presentó el cliente a nuestra sucursal argumentando que no se le había pagado. El personal de la sucursal al consultar el dictamen y observar que el estatus dice “Procede reembolso”, duplicó el pago al cliente.Se trata de una pieza de $500.");
-        parameters.put("operativo", "");
-        parameters.put("operativoAfectacion", "");
-        parameters.put("operativoDetalle", "");
+        parameters.put("fechaEvento", "10 de septiembre de 2019");
+        parameters.put("folioEvento", "2019131");
+        parameters.put("responsableSeguimiento", "Rodríguez Islas Delia Elsa");
+        parameters.put("descripcionEvento","Una hoja no localizada en el lote 69 del tiro 12/19 de la denominación 500 G en el proceso de Numeración. El día 9 de julio en la máquina Super Numerota III, de la Fábrica de Billetes Ciudad de México, durante la impresión del lote 69, se presentó una falla en el sistema Parvis (Sistema de Control de la Producción), esta falla no permitió concluir correctamente con el procesamiento y registro del lote en el sistema. Una vez que se logró restablecer el sistema, se decidió dejar pendiente la impresión del lote 70 hasta el final de tiro.  Después de la impresión del lote 70, último lote numerado, el Aprendiz de Prensista, reportó en la bitácora de la máquina el faltante de una hoja en el lote 69, por lo que no se completaron las 10,000 hojas que integran cada uno de los lotes de producción. " );
+        parameters.put("descripcionCausa", "Una hoja no localizada en el lote 69 del tiro 12/19 de la denominación 500 G en el proceso de Numeración. El día 9 de julio en la máquina Super Numerota III, de la Fábrica de Billetes Ciudad de México, durante la impresión del lote 69, se presentó una falla en el sistema Parvis (Sistema de Control de la Producción), esta falla no permitió concluir correctamente con el procesamiento y registro del lote en el sistema. Una vez que se logró restablecer el sistema, se decidió dejar pendiente la impresión del lote 70 hasta el final de tiro.  Después de la impresión del lote 70, último lote numerado, el Aprendiz de Prensista, reportó en la bitácora de la máquina el faltante de una hoja en el lote 69, por lo que no se completaron las 10,000 hojas que integran cada uno de los lotes de producción. ");
+        parameters.put("descripcionConsecuencia", "Una hoja faltante del lote 69 del tiro 12/19 de la denominación 500 G.  Se requirió de la participación de personal de las Oficinas de Impresión de la Ciudad de México para el recuento de alrededor de 23 lotes.");
+        parameters.put("accionesInmediatas", "15/07/2019- Se recontó el lote 70 para confirmar la cantidad de hojas con las que había terminado el tiro y tratar de ubicar la hoja faltante del lote 69.  16/07/2019- Se recontó el lote 69 para buscar la hoja faltante   16 y 17/07/2018- Se recuentan los lotes que se imprimieron el mismo día que el lote 69, esto son: 66, 67, 71, 72, 73, 74 y 75.   17/07/2019- Se dio aviso a los funcionarios correspondientes.  18/07/2019- Se recontaron los lotes que se imprimieron el día previo y el siguiente día a la impresión del lote 69, los cuales son: 68, 76, 77, 78, 79, 82 y 83.  19/07/2019- Se revisaron los registros de los lotes 69 y 70 en el sistema Parvis para rastrear su paso por todos los procesos de impresión e identificar si alguno de ellos tenía diferencias antes de llegar al proceso de Numeración, y de ser el caso, reorientar la búsqueda de la hoja faltante del lote 69.  22 al 24/07/2019- Se recontaron los lotes del tiro 12/19 de 500 “G” que fueron procesados en días cercanos al día en que se imprimió el lote 69, estos son: 85, 86, 87, 88, 89, 90 y 91.   22/07/2019- Personal de las oficinas de Impresión y de Control de Operaciones de Caja y Fabricación de Billetes, recontaron la maculatura controlada, sin localizar la hoja faltante.  26/07/2019- Se solicitó revisar el interior de la máquina Serigráfica en la que se imprimió el tiro 12/19, para lo cual fue necesario remover las tolvas e inspeccionar áreas de difícil acceso en el interior de la máquina, esto permitió observar que en la parte baja de la sección donde las hojas dan vuelta para pasar por las lámparas de secado UV, se encontraba la hoja faltante de $500G perteneciente al tiro 12/19. ");
         parameters.put("operativo", "X");
-        parameters.put("operativoAfectacion", "leve");
-        parameters.put("operativoDetalle", "Prueba detalle operativo");
+        parameters.put("operativoAfectacion", "Leve");
+        parameters.put("operativoDetalle", "bla bla bla");
         parameters.put("monetario", "");
         parameters.put("monetarioAfectacion", "");
         parameters.put("monetarioDetalle", "");
-        parameters.put("reputacion", "X");
-        parameters.put("reputacionAfectacion", "Grave");
-        parameters.put("reputacionDetalle", "Prueba detalle reputación");
+        parameters.put("reputacion", "");
+        parameters.put("reputacionAfectacion", "");
+        parameters.put("reputacionDetalle", "");
         parameters.put("salud", "");
         parameters.put("saludAfectacion", "");
         parameters.put("saludDetalle", "");
         parameters.put("medioAmbiente", "");
         parameters.put("medioAfectacion", "");
         parameters.put("medioDetalle", "");
+        parameters.put("descripcionAccion1", "Se estará al pendiente de los correos que les haga llegar la Oficina de\nAdministración de esta CR, para que este tipo de evento no se presente.");
+        parameters.put("responsable1", "T43422");
+        parameters.put("fecha1", "19 de julio de 2019");
+        parameters.put("comentarios1", "Mantenimiento entrego la maquina supernumerota 2 el día 25-03-2019 en el turno matutino, iniciando producción el mismo día en el turno vespertino, con ello concluye el evento con un operación normal.");
+        parameters.put("descripcionAccion2", "");
+        parameters.put("responsable2", "");
+        parameters.put("fecha2", "");
+        parameters.put("comentarios2", "");
+        parameters.put("descripcionAccion3", "El día 25 de julio del presente, durante el Acuerdo entre la Dirección General de Emisión, la Dirección de Fabricación de Billetes y la Dirección de Administración de Emisión, el Lic. Alegre Rabiela informó que las gerencias: Informática de la DGE, Gestión de la DGE y Desarrollo y Reingeniería de Procesos participarían en la revisión del proceso de impresión de billetes y los controles existentes en conjunto con la DFB.");
+        parameters.put("responsable3", "K09226");
+        parameters.put("fecha3", "30 de agosto de 2019");
+        parameters.put("comentarios3", "Se concluyó con el análisis del proceso de impresión. En dicho análisis se identificaron los controles existentes en la operación, las áreas de oportunidad y los controles a reforzar en la operación");
+        parameters.put("fechaSeguimiento", "30 de agosto de 2019");
+        parameters.put("estadoAtencion", "Cerrado");
+
         itextR m = new itextR();
         m.generateReport(parameters);
     }
